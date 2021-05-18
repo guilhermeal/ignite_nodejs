5f03e78f7d9616541f6ec4fb4a0b549aa02fc7b7
Guilherme Almeida <guiallan.al@gmail.com>
2021-05-18 03:32:28 -0300

    create middleware to verify if account exists by cpf
    Ref: Chapter I Module 2 Lesson 06 - Middlewares

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ app.use(express.json());
 
 const customers = [];
 
+// Middlewares
+function verifyIfExistsAccountCPF(request, response, next) {
+	const { cpf } = request.headers;
+	
+	const customer = customers.find(customer => customer.cpf === cpf);
+
+	if(!customer) {
+		return response.status(400).json({ error: "Customer not found!" });
+	}
+
+	request.customer = customer;
+	
+	return next();
+}
+
 app.post("/account", (request, response) => {
 	const { cpf, name } = request.body;
 
@@ -30,15 +45,12 @@ app.post("/account", (request, response) => {
 
 });
 
-app.get("/statement", (request, response) => {
-	const { cpf } = request.headers;
-	
-	const customer = customers.find(customer => customer.cpf === cpf);
+// Call Middleware - ALternative
+// app.use(verifyIfExistsAccountCPF);
 
-	if(!customer) {
-		return response.status(400).json({ error: "Customer not found!" });
-	}
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
 
+	const { customer } = request;
 	return response.json(customer.statement);
 
 });
